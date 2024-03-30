c9ff3f48052e681da7cc8012319935ba9877a7b5
Amiri Hayes <89553495+AmiriHayes@users.noreply.github.com>
2024-03-30 15:18:57 -0400

    Create package.m

---
+++ MATLAB_Assistant/package.m
@@ -0,0 +1,10 @@
+classdef package < matlab.Package
+    properties
+        Name = 'MATLAB AI ASSISTANT';
+        Version = '1.0';
+        Description = ['A basic MATLAB package for linking documentation ' ...
+            'and using ChatGPT to answer MATLAB questions'];
+        Author = 'Amiri Hayes';
+        Email = 'amkaha2018@gmail.com';
+    end
+end
