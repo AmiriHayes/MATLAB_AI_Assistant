b99f10a79cff4c975baea3110807d705d0535256
Amiri Hayes <89553495+AmiriHayes@users.noreply.github.com>
2024-03-30 16:20:56 -0400

    Update document.m

--- MATLAB_Assistant/document.m
+++ MATLAB_Assistant/document.m
@@ -1,3 +1,10 @@
-function response = ai_query(prompt)
-  response = 0;
+function documentation = document(matlab_function)
+    if iskeyword(matlab_function)
+            help matlab_function
+            documentation = 1;
+    else
+        disp(matlab_function & "is not a matlab function. Try using ai_query to" + ...
+            "determine the correct function.")
+            documentation = 0;
+    end
 end
